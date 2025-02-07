fdb07ae61a368f93b700596e5193cd63f7f8a24f
ra-bogdanov <ra.bogdanov@northis.ru>
2025-02-07 18:38:42 +0300

    sec commit v2.10

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,3 @@
 export default function About() {
-    return <h1>About Us v2.9</h1>;
+    return <h1>About Us v2.10</h1>;
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-    return <h1>Welcome to Home Page v2.9</h1>;
+    return <h1>Welcome to Home Page v2.10</h1>;
 }
