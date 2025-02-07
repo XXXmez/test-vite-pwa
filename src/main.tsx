7a71d52a5c537a7bb4728d9bce716e6eafac841e
ra-bogdanov <ra.bogdanov@northis.ru>
2025-02-07 18:10:56 +0300

    sec commit v2.1

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {ServiceWorkerProvider} from "./providers/service-worker-provider.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <ServiceWorkerProvider>
+        <StrictMode>
+            <App />
+        </StrictMode>
+    </ServiceWorkerProvider>,
 )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-    return <h1>Welcome to Home Page v2</h1>;
+    return <h1>Welcome to Home Page v2.1</h1>;
 }
