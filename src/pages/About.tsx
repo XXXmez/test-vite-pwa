00e8afef5a101e477718fdb246188858c359186e
ruslan bogdanov <russian.bogd@gmail.com>
2025-02-10 01:10:32 +0300

    sec commit v2.15

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,3 @@
 export default function About() {
-    return <h1>About Us v2.14</h1>;
+    return <h1>About Us v2.15</h1>;
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-    return <h1>Welcome to Home Page v2.14</h1>;
+    return <h1>Welcome to Home Page v2.15</h1>;
 }
