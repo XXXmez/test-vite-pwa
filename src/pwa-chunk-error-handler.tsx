d1197a19039f170ea11d2eaa76627d762e75a756
ra-bogdanov <ra.bogdanov@northis.ru>
2025-02-10 09:10:16 +0300

    sec commit v2.27

--- src/pwa-chunk-error-handler.tsx
+++ src/pwa-chunk-error-handler.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-
 export function PwaChunkErrorHandler({ children }: { children: React.ReactNode }) {
     const nav = useNavigate();
 
