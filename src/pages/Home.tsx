2218c9e2513994e37aee97598216e692ba9d2a96
ra-bogdanov <ra.bogdanov@northis.ru>
2025-05-06 15:09:42 +0300

    feature: pwa init update v0019

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-    return <h1>Welcome to Home Page v0018</h1>;
+    return <h1>Welcome to Home Page v0019</h1>;
 }
--- src/providers/service-worker-provider.tsx
+++ src/providers/service-worker-provider.tsx
@@ -18,9 +18,60 @@ export function useServiceWorkerRegistration() {
     return useContext(ServiceWorkerRegistrationContext);
 }
 
+// async function checkingAppVersionByInit(swUrl: string, registration: ServiceWorkerRegistration) {
+//     try {
+//         console.log('Проверка обновлений при старте приложения');
+//         const resp = await fetch(swUrl, {
+//             cache: 'no-store',
+//             headers: {
+//                 cache: 'no-store',
+//                 'cache-control': 'no-cache',
+//             },
+//         });
+//
+//         if (resp?.status === 200) {
+//             await registration.update();
+//
+//             if (registration.waiting) {
+//                 console.log('Обновление уже доступно, активируем его');
+//                 registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+//                 await caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))));
+//                 window.location.reload();
+//                 return;
+//             }
+//             if (registration.installing) {
+//                 console.log('Установка нового сервис-воркера в процессе, ждем завершения');
+//                 await new Promise((resolve) => {
+//                     registration.installing?.addEventListener('statechange', (event) => {
+//
+//                         console.log('event', event)
+//                         console.log('event.target', event.target)
+//                         // @ts-ignore
+//                         console.log('event.target?.state', event.target?.state)
+//                         // @ts-ignore
+//                         if (event.target?.state === 'installed' && registration.waiting) {
+//                             console.log('Новый сервис-воркер установлен, активируем его');
+//                             registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+//                             // @ts-ignore
+//                             resolve();
+//                         }
+//                     });
+//                 });
+//                 await caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))));
+//                 window.location.reload();
+//                 return;
+//             }
+//             console.log('Обновление не требуется');
+//         }
+//     } catch (e) {
+//         console.error('Ошибка при проверке обновлений при старте приложения:', e);
+//     }
+// }
+
 async function checkingAppVersionByInit(swUrl: string, registration: ServiceWorkerRegistration) {
     try {
         console.log('Проверка обновлений при старте приложения');
+
         const resp = await fetch(swUrl, {
             cache: 'no-store',
             headers: {
@@ -39,20 +90,18 @@ async function checkingAppVersionByInit(swUrl: string, registration: ServiceWork
                 window.location.reload();
                 return;
             }
+
             if (registration.installing) {
                 console.log('Установка нового сервис-воркера в процессе, ждем завершения');
-                await new Promise((resolve) => {
+                await new Promise<void>((resolve) => {
                     registration.installing?.addEventListener('statechange', (event) => {
+                        const target = event.target as ServiceWorker | null;
+                        console.log('event', event);
+                        console.log('target', target);
 
-                        console.log('event', event)
-                        console.log('event.target', event.target)
-                        // @ts-ignore
-                        console.log('event.target?.state', event.target?.state)
-                        // @ts-ignore
-                        if (event.target?.state === 'installed' && registration.waiting) {
+                        if (target?.state === 'installed' && registration.waiting) {
                             console.log('Новый сервис-воркер установлен, активируем его');
                             registration.waiting.postMessage({ type: 'SKIP_WAITING' });
-                            // @ts-ignore
                             resolve();
                         }
                     });
@@ -63,8 +112,8 @@ async function checkingAppVersionByInit(swUrl: string, registration: ServiceWork
             }
             console.log('Обновление не требуется');
         }
-    } catch (e) {
-        console.error('Ошибка при проверке обновлений при старте приложения:', e);
+    } catch (error) {
+        console.error('Возникла ошибка при проверке обновления при старте приложения:', error);
     }
 }
 
@@ -82,7 +131,7 @@ export function ServiceWorkerProvider(props: PropsWithChildren) {
         // @ts-ignore
         onRegisteredSW(swUrl, registration) {
             if (registration) {
-                checkingAppVersionByInit(swUrl, registration)
+                checkingAppVersionByInit(swUrl, registration).then()
 
                 setInterval(async () => {
                     try {
